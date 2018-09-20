5f67acba0ebf7d30023b73959e21699d94c85d43
jetzhouxl <1994259826@qq.com>
2018-09-20 09:09:08 +0800

    jetzhouxl

---
+++ src/main/java/com/team2/entity/User.java
@@ -0,0 +1,35 @@
+package com.team2.entity;
+
+public class User {
+
+    private String userName;
+
+    private String password;
+
+    private int type;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
